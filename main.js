e91b1e32747d71793236f44595fb4bcaf95159a3
Tip Powell <powelltip@gmail.com>
2021-02-08 21:10:25 -0500

    data is being fetched but can't pull it out of the object yet

--- main.js
+++ main.js
@@ -1,32 +1,55 @@
-const url = 'https://itunes.apple.com/search?'
+const url = 'https://itunes.apple.com/search?term='
 const form = document.querySelector('#search-form')
 const resultList = document.querySelector('#result-list')
-const searchInput = document.querySelector('#search-bar').value
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
+    let searchInput = document.querySelector('.search-bar').value
     console.log('searchInput is', searchInput)
-    // search()
+
+    function search() {
+        fetch ('https://itunes.apple.com/search?term=' + searchInput)
+        .then(resp => resp.json())
+        .then (data => {
+            console.log(data)
+            let artistName = data.artistName
+            console.log(artistName)
+        })
+    }
+
+    search()
+
     // const noteText = document.querySelector('#note-text').value
     // const noteTitle = document.querySelector('#note-title').value
     // createNote(noteText, noteTitle)
 })
 
+// fetch(urlBase + from)
+// .then(response => response.json())
+// .then(data => {
+//   let rates = data.rates
+//   console.log(rates[to]) 
+//   calculate(rates[to], amount)
+// })
+// }
 
-function search() {
-    fetch (`https://itunes.apple.com/search?${searchInput}`)
-    .then(resp => resp.json())
-    .then (data => {
-        console.log(data)
-        for (let music of data) {
-            console.log(music)
-            renderMusicItem(music)
-        }
-    })
-}
+// function search() {
+//     fetch ('https://itunes.apple.com/search?term='+ searchInput)
+//     // fetch('https://itunes.apple.com/search?term=owen')
+//     .then(resp => resp.json())
+//     .then (data => {
+//         console.log(data)
+//     })
+// }
+        // for (let music of data) {
+        //     console.log(music)
+        //     renderMusicItem(music)
+//         }
+//     })
+// }
 
-function renderMusicItem(musicObj) {
-    const musicEl = document.createEl('li')
-    // need to get data rendered from data object
-    resultList.appendChild(musicEl)
-}
+// function renderMusicItem(musicObj) {
+//     const musicEl = document.createEl('li')
+//     // need to get data rendered from data object
+//     resultList.appendChild(musicEl)
+// }
